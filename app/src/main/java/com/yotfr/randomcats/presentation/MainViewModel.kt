8ca7405bf77a5bcaf4b7cf338f5f685cdc15b960
Yotfr <yotfr73@gmail.com>
2022-12-04 05:46:08 +0400

    Sign navigation rework animations added

---
+++ app/src/main/java/com/yotfr/randomcats/presentation/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.yotfr.randomcats.presentation
+
+class MainViewModel {
+}
--- app/src/main/java/com/yotfr/randomcats/presentation/screens/splash/event/SplashScreenEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.yotfr.randomcats.presentation.screens.splash.event
-
-sealed interface SplashScreenEvent {
-    object NavigateHome:SplashScreenEvent
-    object NavigateAuth:SplashScreenEvent
-}
